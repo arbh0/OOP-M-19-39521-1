8b62c8c1d231ef787c53026c8508f4355845cde9
arbh0 <arbhoa2@gmail.com>
2021-02-15 13:01:22 +0600

    lab 4 added

--- Laab Task 4/Laab Task 4/Program.cs
+++ Laab Task 4/Laab Task 4/Program.cs
@@ -10,54 +10,65 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[] { 1, 2, 3, 4, 5 };
-            int[] arr2 = new int[] { 6, 7, 8, 9, 0 };
-            int count;
+            /*reverse order array*/
 
-            Console.WriteLine("Original array: ");
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.Write(arr[i] + " ");
-            }
-
-            Console.WriteLine();
-
-            Console.WriteLine("Array in reverse order: ");
-
+            int[] arr = new int[] { 2, 4, 6, 8, 10, 12, 14, 16 };
+            Console.WriteLine("# Reverse Order");
             for (int i = arr.Length - 1; i >= 0; i--)
             {
-                Console.Write(arr[i] + " ");
-            }
-
-            {
-                var sum = 0;
-                for (var i = 0; i < arr.Length; i++)
-                {
-                    sum += arr[i];
-                }
-                Console.WriteLine("\nArray Sum: " + sum);
-            }
-            for (int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = arr2[i];
-            }
-            for (int k = 0; k < arr2.Length; k++)
-            {
-                Console.WriteLine(arr2[k] + "");
+                Console.WriteLine(arr[i]);
             }
             Console.WriteLine();
 
-            for (int j = 0; j < arr.Length - 1; j++)
-            {
+             /* Sum of all elements of the array */
 
-                if (arr[j] == arr[j + 1])
-                    count = count + 1;
-            }
-            Console.WriteLine("\t\n " + arr[i] + "occurse" + count);
-            Console.ReadKey();
+            int[] arr1 = new int[] { 2, 4, 6, 8, 10, 12, 14, 16 };
+            int sum = 0;
+            for (int i = 0; i < arr1.Length; i++)
+                sum += arr1[i];
+            Console.WriteLine("# Sum of all elements of the array: {0}", sum);
+            Console.WriteLine();
+
+            /* Copy the element of one array to another array */
+
+            int[] arr2 = new int[] { 2, 4, 6, 8, 10, 12, 14, 16 };
+            int[] arrtmp = new int[arr2.Length];
+            for (int i = 0; i < arr2.Length; i++)
+                arrtmp[i] = arr2[arr2.Length - 1 - i];
+
+            Console.WriteLine("# Copy the element of one array to another array");
+            for (int i = 0; i < arrtmp.Length; i++)
+                Console.WriteLine(arrtmp[i]);
+            Console.WriteLine();
+            
+            /* Count total number of duplicate elements of the array */
+            
+            int[] arr3 = new int[] { 4, 5, 8, 6, 7, 6, 4, 2, 7, 9 };
+            int max = 0;
+            for (int i = 0; i < arr3.Length; i++)
+                if (arr3[i] > max)
+                    max = arr3[i];
+            int[] arrCount = new int[max + 1];
+
+            for (int i = 0; i < arrCount.Length; i++)
+                arrCount[i] = 0;
+
+            for (int i = 0; i < arr3.Length; i++)
+                arrCount[arr3[i]]++;
+
+            Console.WriteLine("# Count total number of duplicate elements of the array");
+            for (int i = 0; i < arrCount.Length; i++)
+                if (arrCount[i] > 1)
+                    Console.WriteLine("{0} is {1} times", i, arrCount[i]);
+            Console.WriteLine();
+
+            /* Unique elements in an Array */
+
+            Console.WriteLine("# Unique elements in an Array");
+            for (int i = 0; i < arrCount.Length; i++)
+                if (arrCount[i] == 1)
+                    Console.WriteLine("{0} is {1} times", i, arrCount[i]);
+            Console.ReadLine();
         }
-
-
-
     }
 }
