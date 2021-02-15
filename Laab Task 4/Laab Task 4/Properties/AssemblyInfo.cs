40637a073bb3fc2fda9f0db857edfdd49acd5fc4
arbh0 <arbhoa2@gmail.com>
2021-02-15 11:28:05 +0600

    Lab 4 added

--- Laab Task 4/Laab Task 4/Properties/AssemblyInfo.cs
+++ Laab Task 4/Laab Task 4/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("]Lab Task 2")]
+[assembly: AssemblyTitle("Laab Task 4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("]Lab Task 2")]
+[assembly: AssemblyProduct("Laab Task 4")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8b8927da-34fe-4fbb-9ad4-62f53fd90932")]
+[assembly: Guid("d1ec6f1a-ac3b-47b4-b4c8-ecb7fe8b5487")]
 
 // Version information for an assembly consists of the following four values:
 //
